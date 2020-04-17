9fd0fca1fa603be52ea5745e0bebf38283d4f926
welbertFC <welbert.jsj@gmail.com>
2020-04-17 01:34:14 -0300

    update settingr3

--- school/settings.py
+++ school/settings.py
@@ -38,7 +38,7 @@ EMAIL_HOST_USER = os.environ.get ('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get ('EMAIL_HOST_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['apitesteuna.herokuapp.com']
 
